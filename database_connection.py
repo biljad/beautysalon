b6c0ef3b539d5845c6cb19795aa2ff197372e84f
Biljana Drljaca <biljana.drljaca@outlook.com>
2023-01-25 21:21:34 +0100

    Fixed table client

--- database_connection.py
+++ database_connection.py
@@ -1,5 +1,5 @@
 import sqlite3
-connection = sqlite3.connect("database.db")
+connection = sqlite3.connect("beauty_salon.db")
 cursor = connection.cursor()
 sql_command = """CREATE TABLE employee (
 ID INTEGER PRIMARY KEY,
@@ -12,14 +12,14 @@ work_position VARCHAR(100));"""
 cursor.execute(sql_command)
 connection.close()
 
-connection = sqlite3.connect("database.db")
+connection = sqlite3.connect("beauty_salon.db")
 cursor = connection.cursor()
 sql_command = """CREATE TABLE client (
 ID INTEGER PRIMARY KEY,
 JMBG INTEGER ,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
-date_of_birth DATE;"""
+date_of_birth DATE)"""
 cursor.execute(sql_command)
 connection.close()
 
