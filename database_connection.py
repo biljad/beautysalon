d4fa46b7050d9c49a2f241f517f55710c9ec8c1f
Biljana Drljaca <biljana.drljaca@outlook.com>
2023-01-25 20:56:00 +0100

    Add new table Client.

--- database_connection.py
+++ database_connection.py
@@ -12,6 +12,15 @@ work_position VARCHAR(100));"""
 cursor.execute(sql_command)
 connection.close()
 
-
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+sql_command = """CREATE TABLE client (
+ID INTEGER PRIMARY KEY,
+JMBG INTEGER ,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+date_of_birth DATE;"""
+cursor.execute(sql_command)
+connection.close()
 
 
