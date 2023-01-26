abd8399b4bed7519c75f91e46abc8399b55acb9e
Biljana Drljaca <biljana.drljaca@outlook.com>
2023-01-26 15:25:51 +0100

    Added new table Services.

--- database_connection.py
+++ database_connection.py
@@ -23,4 +23,18 @@ date_of_birth DATE)"""
 cursor.execute(sql_command)
 connection.close()
 
+connection = sqlite3.connect("beauty_salon.db")
+cursor = connection.cursor()
+sql_command = """CREATE TABLE IF NOT EXISTS services (
+ID INTEGER PRIMARY KEY,
+nail_extension VARCHAR(30) ,
+nail_correction VARCHAR(30),
+permanent_varnish VARCHAR(30),
+make_up VARCHAR(30),
+waxing VARCHAR(30))"""
+cursor.execute(sql_command)
+connection.close()
+
+
+
 
